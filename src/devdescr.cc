5fdef1d688cc6de6b7efbfa65de08145a95db001
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2006-05-24 15:41:28 +0000

    Add device descriptor entries for ATmega2560/2561.
    git-svn-id: svn://svn.code.sf.net/p/avarice/code/trunk/avarice@136 a7e36acc-7495-4a18-91ca-7b9a7881be12

--- src/devdescr.cc
+++ src/devdescr.cc
@@ -1126,6 +1126,116 @@ jtag_device_def_type deviceDefinitions[] = {
 	    fill_b2(0x3F),		// EECRAddress
 	},
     },
+    // DEV_ATMEGA_2560
+    {
+        "atmega2560",
+        0x9801,
+        256, 1024,   // 256K flash
+        8,   512,    // 4K bytes EEPROM
+        0xe4,        // 57 interrupt vectors
+	DEVFL_MKII_ONLY,
+        atmega128_io_registers,	// XXX should be changed
+        {
+	  0			// no mkI support
+        },
+	{
+	    CMND_SET_DEVICE_DESCRIPTOR,
+	    { 0xFF,0xFF,0xFF,0xFF,0xFF,0x3D,0xB9,0xF8 }, // ucReadIO
+	    { 0 },			// ucReadIOShadow
+	    { 0xFF,0xFF,0x1F,0xE0,0xFF,0x1D,0xA9,0xF8 }, // ucWriteIO
+	    { 0 },			// ucWriteIOShadow
+	    { 0x73,0xFF,0x3F,0xFF,0xF7,0x3F,0xF7,0x3F,
+	      0xF7,0x3F,0x5F,0x3F,0x37,0x37,0x37,0x00,
+	      0x00,0x00,0x00,0x00,0xFF,0x0F,0x00,0x00,
+	      0xF7, 0x3F, 0x37 }, // ucReadExtIO
+	    { 0 },			// ucReadIOExtShadow
+	    { 0x73,0xFF,0x3F,0xF8,0xF7,0x3F,0xF7,0x3F,
+	      0xF7,0x3F,0x5F,0x2F,0x36,0x36,0x36,0x00,
+	      0x00,0x00,0x00,0x00,0xFF,0x0F,0x00,0x00,
+	      0xF7, 0x3F, 0x36 }, // ucWriteExtIO
+	    { 0 },			// ucWriteIOExtShadow
+	    0x31,			// ucIDRAddress
+	    0x57,			// ucSPMCRAddress
+	    0x3B,			// ucRAMPZAddress
+	    fill_b2(256),		// uiFlashPageSize
+	    8,				// ucEepromPageSize
+	    fill_b4(0x1FE00),		// ulBootAddress
+	    fill_b2(0x0136),		// uiUpperExtIOLoc
+	    fill_b4(0x40000),		// ulFlashSize
+	    { 0 },			// ucEepromInst
+	    { 0 },			// ucFlashInst
+	    0x3e,			// ucSPHaddr
+	    0x3d,			// ucSPLaddr
+	    fill_b2(0x40000 / 256),	// uiFlashpages
+	    0,				// ucDWDRAddress
+	    0,				// ucDWBasePC
+	    0,				// ucAllowFullPageBitstream
+	    fill_b2(0),			// uiStartSmallestBootLoaderSection
+	    1,				// EnablePageProgramming
+	    0,				// ucCacheType
+	    fill_b2(0x100),		// uiSramStartAddr
+	    0,				// ucResetType
+	    0,				// ucPCMaskExtended
+	    0,				// ucPCMaskHigh
+	    0,				// ucEindAddress
+	    fill_b2(0x3F),		// EECRAddress
+	},
+    },
+    // DEV_ATMEGA_2561
+    {
+        "atmega2561",
+        0x9802,
+        256, 1024,   // 256K flash
+        8,   512,    // 4K bytes EEPROM
+        0xe4,        // 57 interrupt vectors
+	DEVFL_MKII_ONLY,
+        atmega128_io_registers,	// XXX should be changed
+        {
+	  0			// no mkI support
+        },
+	{
+	    CMND_SET_DEVICE_DESCRIPTOR,
+	    { 0xFF,0xFF,0xFF,0xFF,0xFF,0x3D,0xB9,0xF8 }, // ucReadIO
+	    { 0 },			// ucReadIOShadow
+	    { 0xFF,0xFF,0x1F,0xE0,0xFF,0x1D,0xA9,0xF8 }, // ucWriteIO
+	    { 0 },			// ucWriteIOShadow
+	    { 0x73,0xFF,0x3F,0xFF,0xF7,0x3F,0xF7,0x3F,
+	      0xF7,0x3F,0x5F,0x3F,0x37,0x37,0x37,0x00,
+	      0x00,0x00,0x00,0x00,0xFF,0x0F,0x00,0x00,
+	      0xF7, 0x3F, 0x37 }, // ucReadExtIO
+	    { 0 },			// ucReadIOExtShadow
+	    { 0x73,0xFF,0x3F,0xF8,0xF7,0x3F,0xF7,0x3F,
+	      0xF7,0x3F,0x5F,0x2F,0x36,0x36,0x36,0x00,
+	      0x00,0x00,0x00,0x00,0xFF,0x0F,0x00,0x00,
+	      0xF7, 0x3F, 0x36 }, // ucWriteExtIO
+	    { 0 },			// ucWriteIOExtShadow
+	    0x31,			// ucIDRAddress
+	    0x57,			// ucSPMCRAddress
+	    0x3B,			// ucRAMPZAddress
+	    fill_b2(256),		// uiFlashPageSize
+	    8,				// ucEepromPageSize
+	    fill_b4(0x1FE00),		// ulBootAddress
+	    fill_b2(0x0136),		// uiUpperExtIOLoc
+	    fill_b4(0x40000),		// ulFlashSize
+	    { 0 },			// ucEepromInst
+	    { 0 },			// ucFlashInst
+	    0x3e,			// ucSPHaddr
+	    0x3d,			// ucSPLaddr
+	    fill_b2(0x40000 / 256),	// uiFlashpages
+	    0,				// ucDWDRAddress
+	    0,				// ucDWBasePC
+	    0,				// ucAllowFullPageBitstream
+	    fill_b2(0),			// uiStartSmallestBootLoaderSection
+	    1,				// EnablePageProgramming
+	    0,				// ucCacheType
+	    fill_b2(0x100),		// uiSramStartAddr
+	    0,				// ucResetType
+	    0,				// ucPCMaskExtended
+	    0,				// ucPCMaskHigh
+	    0,				// ucEindAddress
+	    fill_b2(0x3F),		// EECRAddress
+	},
+    },
     // DEV_AT90USB128x
     {
         "at90usb1287",
